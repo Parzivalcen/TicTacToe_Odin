74b82d4e75c41f1253a1eee7e5dd096b09dd38b8
David Centurion <dencen9@gmail.com>
2022-05-19 17:19:49 -0400

    change how the symbols are added, creating just one object from wich to create players

--- app.js
+++ app.js
@@ -8,51 +8,73 @@ const game = (() => {
     const positionIndex = (index) => _boardArr[index];
     return { getBoard, positionIndex };
   })();
-  // players take row and column of where to put the symbol
-  // How do you create player tho
-  const players = (() => {
-    
-    const player1 = (position) => {
-      gameboard.getBoard()[position] = "X"; // Where to put X
-      
-    };
-    const player2 = (position) => {
-      gameboard.getBoard()[position] = "O"; // Where to put O
-      
-    };
-    
-    return { player1, player2 };
-  })();
+
+  // get the name of players from HTML document.
+  const getName = (player) => {
+    let name
+      name = document.querySelector(`#name-${player}`).value;
+      return name; 
+  }
+
+  // Players Contructor
+  const player = (number) => {
+    let name = () => getName(number); 
+    let id = number; 
+    const play = (position) =>{
+      if (number === 1)
+      {
+        gameboard.getBoard()[position] = 'X';
+      }else if (number === 2)
+      {
+        gameboard.getBoard()[position] = 'O';
+      }
+    }
+    return {name, play};
+  }
+
 
   const displayController = (() => {
     // write to DOM
+    
     // What to do when a box is clicked
     const click = (X, O) => {
+      // Create Players
+      const player1 = player(1)
+      const player2 = player(2)
+
+      // Changes when players start playing. 
       let _played1 = false;
       let _played2 = false;
       // Changes when ther is a winner so you can't play anymore
       let _emptyBox = '';
       document.querySelector('.board-container').addEventListener("click", (e) => {
         let symbolHTML = e.target.firstChild;
+        
         if (symbolHTML.classList.contains("symbol") && symbolHTML.innerHTML === _emptyBox) {
           // get box index to populate board Array
           let box = symbolHTML.getAttribute("data-box");
+
           // check if player has played, if not, allow to play until there is a winner
-          if (_played1) {
+          if (_played1) 
+          {
             e.target.firstChild.innerHTML = O;
             // populate board Array
-            players.player2(box);
+            player2.play(box);
             _played2 = true;
             _played1 = false;
-          } else {
+          } 
+          else 
+          {
             e.target.firstChild.innerHTML = X;
             // populate board Array
-            players.player1(box);
+            player1.play(box);
             _played1 = true;
             _played2 = false;
           }
+
           // print winner on screen.
-          if (checkWinner().winner || checkWinner().winner == 'tie' ) {
+          if (checkWinner().winner || checkWinner().winner == 'tie' ) 
+          {
             const win = document.createElement("h1");
             win.innerHTML = checkWinner().message;
             document.querySelector(".main").appendChild(win);
@@ -60,7 +82,6 @@ const game = (() => {
             _emptyBox = 'winner';
           }
           
-         
           // LOG ARRAY
           console.log(gameboard.getBoard());
         }
@@ -83,6 +104,7 @@ const game = (() => {
     ];
     let winner = false;
     let message = "No one won yet";
+    // Check the position of the _winCombos Arr in the game array, if the three indexes of _wincombos have the same symbol, there is a winner. 
     let _player1Wins = _winCombos.some((comboIndex) => {
       return comboIndex.every((index) => gameboard.positionIndex(index) === 'X')
     });
@@ -112,11 +134,12 @@ const game = (() => {
 
     return { message, winner };
   };
+
   displayController.click("x", "O");
   console.log(gameboard.getBoard());
 
 
 
-  return { players };
+  return;
 })();
 // The only thing <<player>> should be able to do is call the function play (to place the items on the specified array index)
